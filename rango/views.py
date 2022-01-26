c14618dce0c552c2a223a4bb80a923041a6a4bae
George Yarr <2553638y@student.gla.ac.uk>
2022-01-26 19:56:34 +0000

    uptoch5

--- rango/views.py
+++ rango/views.py
@@ -14,4 +14,6 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page."+"\n"+"<a href='/rango/'>Index</a>")
+    context_dict = {
+        'boldmessage': 'This tutorial has been put together by George.'}
+    return render(request, 'rango/about.html', context=context_dict)
